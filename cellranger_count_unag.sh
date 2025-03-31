19a7ed4fe1c726b3a5eec342fd87883298e31fef
matt-gust <matt.g0017@gmail.com>
2025-03-31 16:34:09 -0400

    fixed directory structure in egfp batch script and wrote batch script for unag

--- cellranger_count_unag.sh
+++ cellranger_count_unag.sh
@@ -12,21 +12,20 @@
 set -e
 ml cellranger
 #make array of values
-# sra_array=(SRR26705626 SRR26705627 SRR26705629)
-sra_array=(SRR26705628 SRR26705630 SRR26705631)
+sra_array=(SRR29428580 SRR29428581 SRR29428582 SRR29428583 SRR29428584 SRR29428585 SRR29428586 SRR29428587)
 sra_id=${sra_array[$SLURM_ARRAY_TASK_ID]}
 
-mv counts/GSE247228/output/${SLURM_JOBID}.txt counts/GSE247228/output/${sra_id}.txt
+mv counts/GSE179077/output/${SLURM_JOBID}.txt counts/GSE179077/output/${sra_id}.txt
 
 echo Running cellranger count for $sra_id
 
 #Run cellranger
 cellranger count \
     --id ${sra_id} \
-    --output-dir counts/GSE247228/${sra_id} \
-    --fastqs input/fastqs/GSE247228/${sra_id}/ \
+    --output-dir counts/GSE179077/${sra_id} \
+    --fastqs input/fastqs/GSE179077/${sra_id}/ \
     --localcores 20 \
-    --transcriptome custom_ref/eGFP/demo_eGFP \
+    --transcriptome custom_ref/unag/unag_ref \
     --nosecondary \
     --disable-ui \
     --create-bam true
