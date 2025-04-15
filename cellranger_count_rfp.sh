4eb5ad322f83e033fa8a29bbe7718f3693a32139
matt-gust <matt.g0017@gmail.com>
2025-04-15 10:36:27 -0400

    fixed GSE201615 analysis to include multiple runs per sample

--- cellranger_count_rfp.sh
+++ cellranger_count_rfp.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=mk_counts_%j
 #SBATCH --output=counts/GSE201615/output/%j.txt
 #SBATCH --error=counts/GSE201615/output/%j.txt
-#SBATCH --array=0-3
+#SBATCH --array=0-1
 #SBATCH --cpus-per-task=20
 #SBATCH --partition=himem,general
 #SBATCH --time=2-00:00:00
@@ -11,7 +11,7 @@
 
 set -e
 ml cellranger
-sra_array=(SRR18932562 SRR18932563 SRR18932566 SRR18932567)
+sra_array=(GSM6068645 GSM6068647)
 sra_id=${sra_array[$SLURM_ARRAY_TASK_ID]}
 
 mv counts/GSE201615/output/${SLURM_JOBID}.txt counts/GSE201615/output/${sra_id}.txt
