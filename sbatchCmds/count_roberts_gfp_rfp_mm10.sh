cfdc141b2a39a768429221ae1eab48132e3478c8
MVesuviusC <matthewvc1@gmail.com>
2025-08-18 15:07:02 -0400

    Added code for getting the Roberts lab data aligned to the reference including gpf/rfp

---
+++ sbatchCmds/count_roberts_gfp_rfp_mm10.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#SBATCH --account=gdrobertslab
+#SBATCH --output=slurmOut/recount-%j.out
+#SBATCH --error=slurmOut/recount-%j.out
+#SBATCH --job-name=recount
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --partition=general,himem
+#SBATCH --array=0-3
+#SBATCH --wait
+
+set -e ### stops bash script if line ends with error
+
+echo ${HOSTNAME} ${SLURM_ARRAY_TASK_ID}
+
+ml purge
+
+export PATH=/gpfs0/home2/gdrobertslab/lab/Tools/10x/cellranger-7.2.0:$PATH
+
+sample_name_array=(S0122 S0123 S0260 S0261)
+this_sample=${sample_name_array[$SLURM_ARRAY_TASK_ID]}
+
+cellranger count \
+    --id ${this_sample} \
+    --output-dir input/counts/${this_sample} \
+    --fastqs input/fastqs/${this_sample}/ \
+    --localcores 10 \
+    --transcriptome custom_ref/gfp_rfp_mm10 \
+    --nosecondary \
+    --disable-ui
---
+++ sbatchCmds/make_Roberts_fastqs.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#SBATCH --account=gdrobertslab
+#SBATCH --output=slurmOut/mkfastq-%j.out
+#SBATCH --error=slurmOut/mkfastq-%j.out
+#SBATCH --job-name=mkfastq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=general,himem
+#SBATCH --array=0-3
+#SBATCH --wait
+
+set -e ### stops bash script if line ends with error
+
+echo ${HOSTNAME} ${SLURM_ARRAY_TASK_ID}
+
+ml purge
+ml load Miniconda3/4.9.2
+
+export PATH=/gpfs0/home2/gdrobertslab/lab/Tools/10x/cellranger-7.2.0:$PATH
+
+sample_name_array=(S0122 S0123 S0260 S0261)
+this_sample=${sample_name_array[$SLURM_ARRAY_TASK_ID]}
+# Matt deleted the fastq's to create space, but we can recreate the FASTQ using bamtofastq from 10x.
+
+cellranger \
+    bamtofastq \
+    --nthreads=8 \
+    /home/gdrobertslab/lab/Counts_2/${this_sample}/possorted_genome_bam.bam \
+    input/fastqs/${this_sample}
+
+rename \
+    bamtofastq \
+    ${this_sample} \
+    input/fastqs/${this_sample}/*/*.fastq.gz
+
+mv \
+    input/fastqs/${this_sample}/*/*.fastq.gz \
+    input/fastqs/${this_sample}/
