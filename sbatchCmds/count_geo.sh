ae689659cc3c914d80b922a3515856684b5c03a4
MVesuviusC <matthewvc1@gmail.com>
2025-08-18 14:58:01 -0400

    Redoing how I download and process the GEO datasets.

--- sbatchCmds/count_geo.sh
+++ sbatchCmds/count_geo.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
-#SBATCH --output=input/othertumors/slurmout/two_prostate_count-%j.out
-#SBATCH --error=input/othertumors/slurmout/two_prostate_count-%j.out
-#SBATCH --job-name=prostate_count
+#SBATCH --output=input/othertumors/slurmout/geo_count-%j.out
+#SBATCH --error=input/othertumors/slurmout/geo_count-%j.out
+#SBATCH --job-name=geo_count
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=himem,general
-#SBATCH --array=0-1
 #SBATCH --wait
 
 set -e
 
-sample_array=(HMP11_A
-              HMP11_B)
+sample_array=($( \
+    cut -f 3,8 misc/validation_geo_metadata.txt \
+    | uniq \
+    | grep -v sample_id \
+    | cut -f 1\
+))
+
+ref_array=($( \
+    cut -f 3,8 misc/validation_geo_metadata.txt \
+    | uniq \
+    | grep -v sample_id \
+    | cut -f 2
+))
 
 this_sample=${sample_array[${SLURM_ARRAY_TASK_ID}]}
+this_ref=${ref_array[${SLURM_ARRAY_TASK_ID}]}
+
+echo ${this_sample} ${this_ref}
 
 if [ ! -d output/cellranger_out/${this_sample} ]
 then
     mkdir -p output/cellranger_out/${this_sample}
 fi
-
-RefFolder=/home/gdrobertslab/lab/GenRef
 
 export PATH=/gpfs0/home2/gdrobertslab/lab/Tools/10x/cellranger-7.2.0:$PATH
 
@@ -29,9 +40,12 @@ cellranger count \
   --id ${this_sample} \
   --fastqs input/othertumors/fastqs/${this_sample}/ \
   --sample ${this_sample} \
-  --transcriptome ${RefFolder}/10x-hg38 \
+  --transcriptome ${this_ref} \
   --localcores 10 \
   --expect-cells 8000 \
   --nosecondary \
   --disable-ui \
   --output-dir output/cellranger_out/${this_sample}
+
+# Remove fastqs to save space
+#rm input/othertumors/fastqs/${this_sample}/*.fastq.gz
---
+++ sbatchCmds/get_geo_fastqs.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+#SBATCH --output=input/othertumors/slurmout/get_geo_fastq-%j.out
+#SBATCH --error=input/othertumors/slurmout/get_geo_fastq-%j.out
+#SBATCH --job-name=get_geo_fastq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=himem,general
+#SBATCH --wait
+
+set -e
+
+sras_dl=($(cut -f 2 misc/validation_geo_metadata.txt | grep -v sra_number))
+
+sample_array=($(cut -f 3 misc/validation_geo_metadata.txt | grep -v sample_id))
+
+lane_array=($(cut -f 7 misc/validation_geo_metadata.txt | grep -v fake_lane))
+
+this_sra=${sras_dl[${SLURM_ARRAY_TASK_ID}]}
+this_sample=${sample_array[${SLURM_ARRAY_TASK_ID}]}
+this_lane=${lane_array[${SLURM_ARRAY_TASK_ID}]}
+
+echo ${this_sra} ${this_sample} ${this_lane} ${SLURM_ARRAY_TASK_ID}
+
+if [ ! -d input/othertumors/fastqs/${this_sample} ]
+then
+    mkdir -p input/othertumors/fastqs/${this_sample}
+fi
+
+prefetch ${this_sra} \
+    -C yes \
+    --max-size 300g \
+    -O input/othertumors/fastqs/${this_sample}
+
+fastq-dump \
+    --outdir input/othertumors/fastqs/${this_sample} \
+    --split-files \
+    input/othertumors/fastqs/${this_sample}/${this_sra}/${this_sra}.sra
+# _1 is index
+# _2 is R1 bc/umi
+# _3 is R2 align
+
+# For G0014
+# _1 is R1
+# _2 is R2
+
+pigz input/othertumors/fastqs/${this_sample}/${this_sra}*.fastq
+
+# need to rename the files:
+# https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/using/fastq-input
+# [Sample Name]_S1_L00[Lane Number]_[Read Type]_001.fastq.gz
+# make lane number according to the order of SRR entries
+
+if [ -f input/othertumors/fastqs/${this_sample}/${this_sra}_3.fastq.gz ]
+then
+    echo _1 is index, _2 is R1 _3 is R2
+    mv \
+        input/othertumors/fastqs/${this_sample}/${this_sra}_2.fastq.gz \
+        input/othertumors/fastqs/${this_sample}/${this_sample}_S1_L00${this_lane}_R1_001.fastq.gz
+
+    mv \
+        input/othertumors/fastqs/${this_sample}/${this_sra}_3.fastq.gz \
+        input/othertumors/fastqs/${this_sample}/${this_sample}_S1_L00${this_lane}_R2_001.fastq.gz
+else
+    echo _1 is R1 _2 is R2
+    mv \
+        input/othertumors/fastqs/${this_sample}/${this_sra}_1.fastq.gz \
+        input/othertumors/fastqs/${this_sample}/${this_sample}_S1_L00${this_lane}_R1_001.fastq.gz
+
+    mv \
+        input/othertumors/fastqs/${this_sample}/${this_sra}_2.fastq.gz \
+        input/othertumors/fastqs/${this_sample}/${this_sample}_S1_L00${this_lane}_R2_001.fastq.gz
+fi
+
+rm -r input/othertumors/fastqs/${this_sample}/${this_sra}/${this_sra}.sra
