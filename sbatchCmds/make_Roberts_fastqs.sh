aeb717d1f02722aedf7dddaea53319f789b7800a
MVesuviusC <matthewvc1@gmail.com>
2025-08-22 16:30:33 -0400

    Updated roberts fastq making to use metadata file

--- sbatchCmds/make_Roberts_fastqs.sh
+++ sbatchCmds/make_Roberts_fastqs.sh
@@ -19,7 +19,11 @@ ml load Miniconda3/4.9.2
 
 export PATH=/gpfs0/home2/gdrobertslab/lab/Tools/10x/cellranger-7.2.0:$PATH
 
-sample_name_array=(S0122 S0123 S0260 S0261)
+sample_array=($( \
+    cut -f 1 misc/mouse_samples_Roberts_realign_metadata.tsv \
+    | grep -v Sample_ID \
+))
+
 this_sample=${sample_name_array[$SLURM_ARRAY_TASK_ID]}
 # Matt deleted the fastq's to create space, but we can recreate the FASTQ using bamtofastq from 10x.
 
