c5c2e8e8fc1cc34f59731589eb5163a21a169f38
kkatieshort <kkatieshort@gmail.com>
2026-08-03 09:22:51 -0400

    writing samples as an array & formatting for neatness

--- scripts/compare_snps/2_align_exome_reference.sh
+++ scripts/compare_snps/2_align_exome_reference.sh
@@ -14,13 +14,21 @@ module load bwa-mem2/2.2.1 SAMtools/1.15
 
 wd_path=/home/gdrobertslab/lab/Analysis/Katie/24_validate_snvs
 
+# F420 cell line exome
+# K7M2 cell line exome
+# Balbc exome
+# B6 exome
 
-sample=(
-    SRR13611861 # F420 cell line exome
-    SRR13611860 # K7M2 cell line exome
-    SRR27799014 # Balbc exome
-    SRR27799015 # B6 exome
+sample_array=(
+    SRR13611861
+    SRR13611860
+    SRR27799014
+    SRR27799015
 )
+
+sample=${sample_array[${SLURM_ARRAY_TASK_ID}]}
+
+echo ${sample}
 
 # first align to reference mm10, BL6
 ## ran bwa-mem2 index on the mm10.fa separately; this makes the bwa-mem2 index files
@@ -36,12 +44,24 @@ sample=(
 
 # then, get stats!
 
-bwa-mem2 mem -M -t 2 \
+bwa-mem2 mem \
+        -M -t 10 \
     $wd_path/input/reference/reference \
-    $wd_path/input/exome/${sample}_1.fastq $wd_path/input/exome/${sample}_2.fastq \
-    | samtools fixmate -m -@ 2 - - \
-    | samtools sort -@ 2 -m 2G -T $wd_path/output/bwa/tmp_${sample} - \
-    | samtools markdup -@ 2 --write-index - $wd_path/output/bwa/${sample}_markdup.bam
+    $wd_path/input/exome/${sample}_1.fastq \
+    $wd_path/input/exome/${sample}_2.fastq \
+    | samtools fixmate \
+        -m \
+        -@ 5 - - \
+    | samtools sort \
+        -@ 5 \
+        -m 2G \
+        -T \
+        $wd_path/output/bwa/tmp_${sample} - \
+    | samtools markdup \
+        -@ 5 \
+        --write-index \
+        - \
+        $wd_path/output/bwa/${sample}_markdup.bam
 
 samtools flagstat -@ 2 $wd_path/output/bwa/${sample}_markdup.bam \
     > $wd_path/output/bwa/${sample}_flagstat.txt
