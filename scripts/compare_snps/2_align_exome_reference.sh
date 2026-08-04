4f644a252253de0f4a1bab177caa61720e5dcb0d
kkatieshort <kkatieshort@gmail.com>
2026-08-04 10:03:49 -0400

    adding samples to a tsv to read in the many accessions for processing

--- scripts/compare_snps/2_align_exome_reference.sh
+++ scripts/compare_snps/2_align_exome_reference.sh
@@ -14,21 +14,35 @@ module load bwa-mem2/2.2.1 SAMtools/1.15
 
 wd_path=/home/gdrobertslab/lab/Analysis/Katie/24_validate_snvs
 
-# F420 cell line exome
-# K7M2 cell line exome
-# Balbc exome
-# B6 exome
+# read samples from sample tsv
+## -t => skips trailing newline character as it reads each line
+## <() => process substitution; runs command inside () as if it were file
+## cut => extract columns from tsv
+## -f1 => field 1 or first column
+mapfile \
+    -t \
+    sample_types \
+    < \
+    <(cut \
+        -f2 \
+        $wd_path/misc/compare_snps_samples.tsv \
+        | tail -n +2
+    )
 
-sample_array=(
-    SRR13611861
-    SRR13611860
-    SRR27799014
-    SRR27799015
-)
+mapfile \
+    -t \
+    SRR_IDs \
+    < \
+    <(cut \
+        -f2 \
+        $wd_path/misc/compare_snps_samples.tsv \
+        | tail -n +2
+    )
 
-sample=${sample_array[${SLURM_ARRAY_TASK_ID}]}
+sample_type=${sample_types[$SLURM_ARRAY_TASK_ID]}
+accession=${SRR_IDs[$SLURM_ARRAY_TASK_ID]}
 
-echo ${sample}
+echo "Processing $sample_type ($accession)"
 
 # first align to reference mm10, BL6
 ## ran bwa-mem2 index on the mm10.fa separately; this makes the bwa-mem2 index files
@@ -47,8 +61,8 @@ echo ${sample}
 bwa-mem2 mem \
         -M -t 10 \
     $wd_path/input/reference/reference \
-    $wd_path/input/exome/${sample}_1.fastq \
-    $wd_path/input/exome/${sample}_2.fastq \
+    $wd_path/input/exome/${accession}_1.fastq \
+    $wd_path/input/exome/${accession}_2.fastq \
     | samtools fixmate \
         -m \
         -@ 5 - - \
@@ -56,15 +70,15 @@ bwa-mem2 mem \
         -@ 5 \
         -m 2G \
         -T \
-        $wd_path/output/bwa/tmp_${sample} - \
+        $wd_path/output/bwa/tmp_${accession} - \
     | samtools markdup \
         -@ 5 \
         --write-index \
         - \
-        $wd_path/output/bwa/${sample}_markdup.bam
+        $wd_path/output/bwa/${accession}_markdup.bam
 
-samtools flagstat -@ 2 $wd_path/output/bwa/${sample}_markdup.bam \
-    > $wd_path/output/bwa/${sample}_flagstat.txt
+samtools flagstat -@ 2 $wd_path/output/bwa/${accession}_markdup.bam \
+    > $wd_path/output/bwa/${accession}_flagstat.txt
 
-samtools stats -@ 2 $wd_path/output/bwa/${sample}_markdup.bam \
-    > $wd_path/output/bwa/${sample}_stats.txt
+samtools stats -@ 2 $wd_path/output/bwa/${accession}_markdup.bam \
+    > $wd_path/output/bwa/${accession}_stats.txt
