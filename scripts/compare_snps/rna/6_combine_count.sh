e278c380e4f0d71107d2de0c36dbe47a4206c5c2
kkatieshort <kkatieshort@gmail.com>
2026-08-14 11:57:09 -0400

    script to combine all of the count files so its easily accessible

---
+++ scripts/compare_snps/rna/6_combine_count.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=tsv_combine
+#SBATCH --partition=himem
+#SBATCH --output=/home/gdrobertslab/lab/Analysis/Katie/24_validate_snvs/output/rna/vcfs/logs/count/combine_%j.txt
+#SBATCH --error=/home/gdrobertslab/lab/Analysis/Katie/24_validate_snvs/output/rna/vcfs/logs/count/combine_%j.txt
+#SBATCH --mem=100G
+#SBATCH --time=6:00:00
+#SBATCH --cpus-per-task=4
+
+wd_path=/home/gdrobertslab/lab/Analysis/Katie/24_validate_snvs
+
+files=($wd_path/output/rna/count/*/*_count.tsv)
+
+printf "accession\tsample_type\tvariant_count\tcovered_mb\tmutations_per_mb\n" > $wd_path/output/rna/count/combined.tsv
+
+cat "${files[@]}" >> $wd_path/output/rna/count/combined.tsv
